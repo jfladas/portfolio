e2478d1128cdd2fa09ba452929ac2411d6f6a702
Lukas Bucher <lukas.bucherr@gmail.com>
2024-12-24 03:29:45 +0100

    initialize base styles & icons

--- src/main.js
+++ src/main.js
@@ -1,10 +1,17 @@
-import './assets/main.css'
+import './assets/base.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+library.add(faUserSecret)
+
 const app = createApp(App)
+
+app.component('font-awesome-icon', FontAwesomeIcon)
 
 app.use(router)
 
